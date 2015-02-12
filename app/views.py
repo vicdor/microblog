b21d646338c0f92d9e364496b2dbbb8e16b4623a
vicdor <vc1042@berkeley.edu>
2015-02-12 02:36:21 -0800

    test

---
+++ app/views.py
@@ -0,0 +1,17 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname': 'Carlos'}  # fake user
+    posts = [  # fake array of posts
+        { 'author': {'nickname': 'John'}, 
+            'body': 'It\'s a beautiful day!' },
+        { 'author': {'nickname': 'Susan'}, 
+            'body': 'It\'s okay I guess' }
+    ]
+    return render_template("index.html",
+                           title = 'Victor',
+                           user = user,
+                           posts = posts)
